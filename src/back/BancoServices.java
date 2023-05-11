60286aab21f1c5b14961f10ab69f20e9f95e8fc8
Hilton.Thallyson <hilton.machado.088@ufrn.edu.br>
2023-05-11 15:06:28 -0300

    Merging release 1-0 into main

---
+++ src/back/BancoServices.java
@@ -0,0 +1,143 @@
+package back;
+
+
+import java.util.Objects;
+import java.util.Optional;
+
+
+public class BancoServices {
+	Banco banco;
+	
+	public BancoServices(Banco banco) {
+		this.banco = banco;
+	}
+	
+	public boolean criarConta(int numeroDaConta) {
+		
+		if(checarNumeroDaConta(numeroDaConta)){
+			banco.getContas().add(new Conta(numeroDaConta));
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+
+	public Optional<Conta> consultarSaldo (int numeroDaConta) {
+
+		return
+			 this
+			.banco
+			.getContas()
+			.stream()
+			.filter(c -> c.getNumeroDaConta() == numeroDaConta)
+			.findFirst();
+
+	}
+
+	public int debitar(int numeroDaConta, double valor) {
+
+		if(valor <= 0) {
+			return -3;
+		}
+
+		Conta conta = getConta(numeroDaConta);
+		if (Objects.isNull(conta)) {
+			return -1;
+		} else if (conta.getSaldo() < valor) {
+			return -2;
+		}
+
+		conta.decrementarSaldo(valor);
+
+		return 0;
+	}
+
+//	public void verificarSaldo(Scanner sc) {
+//
+//		int numeroDaConta;
+//
+//		boolean isFinished = false;
+//		Conta minhaConta = null;
+//
+//		do {
+//			System.out.println();
+//			System.out.print("Por favor digite o número da sua conta: ");
+//			numeroDaConta = sc.nextInt();
+//			for(int i=0; i<contas.size(); i++) {
+//				if(numeroDaConta == contas.get(i).getNumeroDaConta()) {
+//					minhaConta = contas.get(i);
+//				}
+//			}
+//
+//			if(minhaConta == null) {
+//				System.out.println("Conta não encontrada!!!");
+//				isFinished = false;
+//			}else {
+//				isFinished = true;
+//			}
+//		}while(!isFinished);
+//
+//		System.out.printf("Seu saldo é: R$ %.2f%n", minhaConta.getSaldo());
+//		System.out.println();
+//
+//	}
+
+	public boolean depositarValor(int numeroDaConta,double valor) {
+			
+			for(int i=0; i<banco.getContas().size(); i++) {
+				if(banco.getContas().get(i).getNumeroDaConta() == numeroDaConta) {
+					
+					banco.getContas().get(i).incrementarSaldo(valor);
+					return true;
+					
+				}
+			}
+			return false;
+			
+	}
+	public boolean checarNumeroDaConta(int numeroDaConta) {
+		for(int i=0; i<banco.getContas().size(); i++) {
+			if(banco.getContas().get(i).getNumeroDaConta() == numeroDaConta) {
+				return false;
+				
+			}
+		}
+		return true;
+	}
+
+	private Conta getConta(int numeroDaConta) {
+
+		for (Conta c : banco.getContas()) {
+			if (c.getNumeroDaConta() == numeroDaConta) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+	public int transferir(int numeroDaContaOrigem, int numeroDaContaDestino, double valor) {
+
+		if(valor <= 0) {
+			return -4;
+		}
+
+		Conta contaOrigem = getConta(numeroDaContaOrigem);
+		Conta contaDestino = getConta(numeroDaContaDestino);
+
+		if (Objects.isNull(contaOrigem)) {
+			return -1;
+		} else if (Objects.isNull(contaDestino)) {
+			return -2;
+		} else if(contaOrigem.getSaldo() < valor) {
+			return -3;
+		}
+
+		contaOrigem.decrementarSaldo(valor);
+		contaDestino.incrementarSaldo(valor);
+
+		return 0;
+	}
+	
+	
+}
